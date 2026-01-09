2c0e63ecd1bd3094904dbd6c03f63e0b986c736b
pinank0301 <pinanktrivedi694@gmail.com>
2026-01-09 21:04:05 +0530

    minor bug fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,7 +20,7 @@ export function Hero() {
 
         try {
             const response = await fetch(
-                "http://localhost:8000/v1/api/url/check",
+                "http://be-aware-backend-ma9x.vercel.app/v1/api/url/check",
                 {
                     method: "POST",
                     headers: {
